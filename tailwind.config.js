0d60688d63662ca0f7b8a587e67c35bff35fa36f
almamuntmn <almamuncool88@gmail.com>
2025-07-15 17:07:29 +0600

    2nd

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
